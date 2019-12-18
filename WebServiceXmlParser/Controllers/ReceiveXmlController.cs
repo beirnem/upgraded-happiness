9b776ebacce34d36ffd857b5ed2cbea237c2067b
Marcus Beirne <marcus.beirne@gmail.com>
2019-12-18 22:24:45 +0000

    Set up web api project.

---
+++ WebServiceXmlParser/Controllers/ReceiveXmlController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebServiceXmlParser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiveXmlController : ControllerBase
+    {
+        private readonly ILogger<ReceiveXmlController> _logger;
+
+        public ReceiveXmlController(ILogger<ReceiveXmlController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/ReceiveXml
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ReceiveXml/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ReceiveXml
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ReceiveXml/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
