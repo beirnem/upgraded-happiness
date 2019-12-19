640039831eb07860f2be2ebf357b961c9b6a2bcd
Marcus Beirne <marcus.beirne@gmail.com>
2019-12-19 22:39:32 +0000

    Add Logging Events codes structure

---
+++ WebServiceXmlParser/Core/Interfaces/IParseInputDocument.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServiceXmlParser.Core.Interfaces
+{
+    public class IParseInputDocument
+    {
+    }
+}
---
+++ WebServiceXmlParser/Core/LoggingEvents.cs
@@ -0,0 +1,9 @@
+﻿namespace WebServiceXmlParser.Core
+{
+    public class LoggingEvents
+    {
+        public const int ValidDocument = 0;
+        public const int InvalidCommand = -1;
+        public const int InvalidSite = -2;
+    }
+}
