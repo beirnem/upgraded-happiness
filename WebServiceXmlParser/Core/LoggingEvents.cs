f2259368c752a8c7f850e5a58f557eb9c0fe9aeb
Marcus Beirne <marcus.beirne@gmail.com>
2019-12-19 22:44:27 +0000

    Add logging statuses

---
+++ WebServiceXmlParser/Core/LoggingEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServiceXmlParser.Core
+{
+    public class LoggingEvents
+    {
+        public const int ValidStructure = 0;
+        public const int InvalidCommand = -1;
+        public const int InvalidSite = -2;
+    }
+}
